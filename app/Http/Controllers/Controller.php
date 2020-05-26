562f59955e0e38ed6c14bed1de37f7b66f3491ac
Luis Angel Ortega <angel-ortega12@hotmail.com>
2020-05-25 20:44:38 -0600

    ✨ Se añaden validadores
    git a .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,31 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function validateString(String $string)
+    {
+        $string = htmlentities($string, ENT_QUOTES, "UTF-8");
+        $string = filter_var($string, FILTER_SANITIZE_STRING);
+        return $string;
+    }
+
+    public function validateDatetime(String $originalDate)
+    {
+        $date = date('Y-m-d H:i:s',strtotime($originalDate));
+        return $date;
+    }
+
+    public function validateInt($value){
+        return (int)$value;
+    }
+
+    public function valueToBool($value)
+    {
+        if ($value == null || $value == 'off' || $value == 'false') {
+            $bool = false;
+        } else if ($value == 'on' || $value == 'true'){
+            $bool = true;
+        }
+        return $bool;
+    }
 }
