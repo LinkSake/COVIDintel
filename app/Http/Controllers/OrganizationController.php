cd60a82e2c0f39ac23496abeb5cbafa615f6b1dc
Luis Angel Ortega <angel-ortega12@hotmail.com>
2020-05-26 21:10:33 -0600

    ✨ Se añaden vistas de org
    Se añaden vistas de org

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -8,9 +8,22 @@ use Illuminate\Support\Facades\DB;
 class OrganizationController extends Controller
 {
 
+    public function overview($id)
+    {
+        $org = DB::select('CALL leer_panel_admin(?)', array($id));
+        $cat = DB::select('CALL leer_cat_org');
+        return view('org/overview',[
+            'org' => $org,
+            'cat' => $cat
+            ]);
+    }
+
     public function new()
     {
-        return view('org/detail');
+        $cat = DB::select('CALL leer_cat_org');
+        return view('org/detail',[
+            'cat' => $cat
+            ]);
     }
 
     public function create(Request $request)
@@ -24,6 +37,55 @@ class OrganizationController extends Controller
         $result = DB::select('
         CALL nueva_org(?,?,?,?,?,?,?)
         ', array($name, $address, $email, $phone, $cat, $admin));
-        return redirect()->action('OrganizationController@list');
+        return redirect()->action('OrganizationController@overview');
     }
+
+    public function edit($id)
+    {
+        $org = DB::table('organization')
+            ->where('id_org', '=', $id)
+            ->get();
+        $cat = DB::select('CALL leer_cat_org');
+        return view('org/detail',[
+            'org' => $org,
+            'cat' => $cat
+            ]);
+    }
+
+    public function update(Request $request)
+    {
+        $id = $request->input('id');
+        $name = $this->validateString($request->input('name'));
+        $address = $this->validateString($request->input('address'));
+        $email = $this->validateString($request->input('email'));
+        $phone = $this->validateString($request->input('phone'));
+        $cat = $this->validateInt($request->input('id_cat_org'));
+        $admin = $this->validateInt($request->input('id_admin'));
+        $result = DB::select('
+            CALL editar_org(?,?,?,?,?,?,?)
+        ', array($id, $name, $address, $email, $phone, $cat, $admin));
+        return redirect()->action('OrganizationController@overview');
+    }
+
+    public function remove($id)
+    {
+        $org = DB::table('organization')
+            ->where('id_org', '=', $id)
+            ->get();
+        $cat = DB::select('CALL leer_cat_org');
+        return view('org/delete',[
+            'org' => $org,
+            'cat' => $cat
+           ]);
+    }
+
+    public function delete(Request $request)
+    {
+        $id = $request->input('id');
+        $org = DB::select('
+            CALL eliminar_org(?)
+        ', array($id));
+        return redirect()->action('OrganizationController@new');
+    }
+
 }
---
+++ resources/views/org/delete.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>ELIMINAR</h1>
+@endsection
